7ebc036587ffe59476521abbce7bb4f978c03af8
Anthony Hill <hillan1152@gmail.com>
2019-10-10 19:11:48 -0400

    set up project

---
+++ client/src/reducer/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    playerCard: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type){
+        default:
+            return state
+    }
+}
+
+export default reducer;
