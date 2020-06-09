ba4d939f9685b71c38e2021657a93d45c788203c
Anthony Hill <hillan1152@gmail.com>
2020-06-09 10:59:11 -0400

    imported images

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Route } from "react-router-dom";
+import './less/index.less'
 
 //Components
 import BarList from './components/BarList';
